3f05849b058847d4b7b37c1aba511acea2be5647
zdan <ndholovu@gmail.com>
2025-06-29 09:22:41 +0900

    'LF'

--- mysort.py
+++ mysort.py
@@ -4,7 +4,7 @@ def selection_sort(arr):
         for j in range(i+1,len(arr)):
             if arr[min_idx]>arr[j]:
                 min_idx=j
-                arr[i],arr[min_idx]=arr[min_idx],arr[i]
+        arr[i],arr[min_idx]=arr[min_idx],arr[i]
     return arr
 
 def quick_sort(arr):
